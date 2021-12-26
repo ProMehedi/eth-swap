882bb4c1b3444667ff797da1ba4390afda952ee3
promehedi <mmhasan563@gmail.com>
2021-12-27 01:48:39 +0600

    Deposit Token & Tests

--- src/ethereum/contracts/Exchange.sol
+++ src/ethereum/contracts/Exchange.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "./Token.sol";
+
 contract Exchange {    
   // Variables
   address public feeAccount;
@@ -12,5 +14,8 @@ contract Exchange {
     feeRate = _feeRate;
   }
 
-  // Set the fee account
+  // Deposit Token
+  function depositToken(address _token, uint256 _amount) public payable {
+    Token(_token).transferFrom(msg.sender, address(this), _amount);
+  }
 }
--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -2,6 +2,7 @@
 import chai from 'chai'
 import chaiAsPromised from 'chai-as-promised'
 import { EVMThrow, tokens } from '../utils'
+const Token = artifacts.require('Token')
 const Exchange = artifacts.require('Exchange')
 
 chai.use(chaiAsPromised)
@@ -10,10 +11,13 @@ chai.should()
 contract('Exchange', (accounts) => {
   const deployer = accounts[0]
   const feeAccount = accounts[1]
+  const user1 = accounts[2]
   const feePercent = 10
 
-  let exchange
+  let exchange, token
   beforeEach(async () => {
+    token = await Token.new()
+    token.transfer(user1, tokens(1000), { from: deployer })
     exchange = await Exchange.new(feeAccount, feePercent)
   })
 
@@ -26,6 +30,28 @@ contract('Exchange', (accounts) => {
     it('Tracks the fee rate', async () => {
       const _feeRate = await exchange.feeRate()
       _feeRate.toNumber().should.equal(feePercent)
+    })
+  })
+
+  describe('Depositing tokens', () => {
+    const from = accounts[0]
+    const to = accounts[1]
+
+    let amount, result
+    describe('Successful transfer', () => {
+      let result
+      beforeEach(async () => {
+        amount = tokens(100)
+        await token.approve(exchange.address, amount, { from: user1 })
+        result = await exchange.depositToken(token.address, amount, {
+          from: user1,
+        })
+      })
+
+      it('Tracks the token deposit', async () => {
+        const userBalance = await token.balanceOf(exchange.address)
+        userBalance.toString().should.equal(amount.toString())
+      })
     })
   })
 })
