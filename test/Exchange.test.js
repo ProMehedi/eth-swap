eb85d77d015feeb9282cf9729a88a3a47347116c
promehedi <mmhasan563@gmail.com>
2021-12-27 01:31:02 +0600

    Fix eslint error

--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import chai from 'chai'
 import chaiAsPromised from 'chai-as-promised'
 import { EVMThrow, tokens } from '../utils'
--- utils.js
+++ utils.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 export const tokens = (n) => {
   return new web3.utils.BN(web3.utils.toWei(n.toString(), 'ether'))
 }
